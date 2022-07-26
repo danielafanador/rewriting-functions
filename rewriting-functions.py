f310798c75dec6956f1db8571ab46a40fd2a2eae
Daniel Afanador <danielthemisfits@gmail.com>
2022-07-26 20:55:23 -0300

    Versión comentada del mismo código

--- rewriting-functions.py
+++ rewriting-functions.py
@@ -1,14 +1,14 @@
-a_list = [1, 8, 10, 9, 7]
-print(max(a_list))
+a_list = [1, 8, 10, 9, 7] #Defino una lista de números
+print(max(a_list)) #Imprimo la lista de números arriba definida
 print (' ')
 
-def max (a_list):
-    a_string = 'No max value returned'
+def max (a_list): #Defino una nueva función creada por mí llamada max
+    a_string = 'No max value returned' #Dentro de la nueva función existe una variable llamada a_string
     
-    return a_string
-    del max
+    return a_string #La función debería devolver la variable
+    del max #Borro la función max que ya existe por Default
 
-max_val_test_0 = (max(a_list))
+max_val_test_0 = (max(a_list)) #La variable max_val_test_0 es igual a la función max
     
 print (max(a_list))
 print (' ')
