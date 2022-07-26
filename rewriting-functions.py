f012fd3baedf5c9b1aaec82520596c26353e7c67
Daniel Afanador <danielthemisfits@gmail.com>
2022-07-26 20:49:51 -0300

    Un programa para reescribir funciones

---
+++ rewriting-functions.py
@@ -0,0 +1,16 @@
+a_list = [1, 8, 10, 9, 7]
+print(max(a_list))
+print (' ')
+
+def max (a_list):
+    a_string = 'No max value returned'
+    
+    return a_string
+    del max
+
+max_val_test_0 = (max(a_list))
+    
+print (max(a_list))
+print (' ')
+print (max_val_test_0)
+
